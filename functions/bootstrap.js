e56dd12bc18ab849f3d700e159dbd22da52c34c5
johnbutyno3 <johnbutyno3@gmail.com>
2026-09-17 17:06:38 +0800

    fix: override tool inventory with initial UNDO grant

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,7 +1,8 @@
 const functions = require('./index');
 const gameSession = require('./game_session');
 const life = require('./life');
+const toolInventoryDefaults = require('./tool_inventory_defaults');
 
-Object.assign(functions, gameSession, life);
+Object.assign(functions, gameSession, life, toolInventoryDefaults);
 
 module.exports = functions;
