11a5d66f1939a826ec28e259aed9163fa0f30999
johnbutyno3 <johnbutyno3@gmail.com>
2026-09-17 15:33:08 +0800

    fix: expose life functions through firebase bootstrap

--- functions/bootstrap.js
+++ functions/bootstrap.js
@@ -1,6 +1,7 @@
 const functions = require('./index');
 const gameSession = require('./game_session');
+const life = require('./life');
 
-Object.assign(functions, gameSession);
+Object.assign(functions, gameSession, life);
 
 module.exports = functions;
