adacd831f8b9d847effbe5f488d421c36de9d506
johnbutyno3 <johnbutyno3@gmail.com>
2026-09-18 15:10:13 +0800

    fix: export authoritative life callables

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,7 @@ setGlobalOptions({ region: 'us-central1' });
 
 const db = getFirestore();
 Object.assign(exports, require('./profile'));
+Object.assign(exports, require('./life'));
 
 const MAX_CHAPTER_INDEX = 5;
 const GAME_SESSION_TTL_MS = 2 * 60 * 60 * 1000;
