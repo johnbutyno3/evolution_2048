6da69fe2cb16917dc5c2d6eb201e89704084e9a2
johnbutyno3 <johnbutyno3@gmail.com>
2026-09-23 14:42:54 +0800

    fix: export life state callables

--- functions/index.js
+++ functions/index.js
@@ -1292,3 +1292,6 @@ exports.completeChapter = onCall(async (request) => {
 
 // Export the current server-authoritative restart/abandon session callables.
 Object.assign(exports, require('./game_session'));
+
+// Export the server-authoritative life state callables used by LifeManager.
+Object.assign(exports, require('./life'));
